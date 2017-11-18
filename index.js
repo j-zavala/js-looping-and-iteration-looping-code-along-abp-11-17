1535de6f5529d5e482c5817b745882976f09e3ac
Johnny Zavala <jzavalafgz@gmail.com>
2017-11-18 19:45:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(name) {
   for (let index = 0; index < name.length; index++) {
-    console.log(`Welcome ${name[index]}! You are employee #1.`);
+    console.log(`Welcome ${name[index]}! You are employee #${index}.`);
   }
   return name;
 }
